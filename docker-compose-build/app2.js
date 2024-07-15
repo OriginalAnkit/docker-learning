7d6771d9a23464419466f3b8fc8d28dba67b7435
Ankit <akt.halder@gmail.com>
2024-07-15 09:46:27 +0530

    compose working

---
+++ docker-compose-build/app2.js
@@ -0,0 +1,5 @@
+const app_name=process.env.APP_NAME
+const env=process.env.ENV
+
+console.log(`Running app2 from ${app_name} with ${env}`)
+console.log(process.env)
---
+++ docker-compose-build/service1/app1.js
@@ -0,0 +1,4 @@
+const app_name=process.env.APP_NAME
+const env=process.env.ENV
+
+console.log(`Running app1 from ${app_name} with ${env}`)
